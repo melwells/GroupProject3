ffcbdff8364643d7684ded591f145bdf49410566
melwells <55035851+melwells@users.noreply.github.com>
2020-02-25 11:04:55 -0500

    Movement and Pause Menu
    Movement and pause menu code implemented.

---
+++ GroupProject3/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{ /*
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+          if (isPaused)
+          {
+            Resume();
+          }
+          else
+          {
+            Pause();
+          }
+        }
+    }
+
+    public void Resume()
+    {
+      pauseMenuUI.SetActive(false);
+      Time.timeScale = 1f;
+      isPaused = false;
+      Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+      pauseMenuUI.SetActive(true);
+      Time.timeScale = 0f;
+      isPaused = true;
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None; //unlock mouse for menu
+    }
+
+    public void Menu()
+    {
+      Debug.Log("Main menu loading..");
+      Time.timeScale = 1f;
+      SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+      Debug.Log("Quit");
+      Application.Quit();
+    } */
+}
---
+++ GroupProject3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+  public CharacterController controller; //calls on controller attached to Player
+  public float speed = 12f;
+  public float gravity = -9.81f;
+  public float jumpHeight = 3f;
+
+//checking for whether or not player is touching the ground
+  public Transform groundCheck;
+  public float groundDistance = 0.4f;
+  public LayerMask groundMask;
+
+  Vector3 originalPos;
+  Vector3 velocity; //help with gravity
+  bool isGrounded; //are we on the ground
+
+  void Start()
+  {
+    originalPos = this.transform.position;
+  }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      //checking for if we are on ground.
+      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+      if (isGrounded && velocity.y <0)
+      {
+        velocity.y = -2f;
+      }
+
+
+      //controlling movement
+      float x = Input.GetAxis("Horizontal"); //x axis
+      float z = Input.GetAxis("Vertical"); //z axis
+
+      Vector3 move = transform.right * x + transform.forward * z;
+
+      controller.Move(move * speed * Time.deltaTime);
+
+      //jump
+      if(Input.GetButtonDown("Jump") && isGrounded)
+      {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+      }
+
+      //fall with gravity
+      velocity.y += gravity * Time.deltaTime;
+
+      controller.Move(velocity * Time.deltaTime);
+    }
+
+}
