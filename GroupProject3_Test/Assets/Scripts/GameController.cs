b623acb81920681b5d8af5574fea6b6daabc5c0d
melwells <55035851+melwells@users.noreply.github.com>
2020-03-03 11:57:23 -0500

    Invisible
    Go invisible, however cooldown not working yet.

---
+++ GroupProject3_Test/Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+          Application.Quit();
+          Debug.Log("Quit game");
+        }
+    }
+}
--- GroupProject3_Test/Assets/Scripts/PlayerControllerNewTest.cs
+++ GroupProject3_Test/Assets/Scripts/PlayerControllerNewTest.cs
@@ -8,6 +8,7 @@ public class PlayerControllerNewTest : MonoBehaviour
     private Vector3 lastMove;
     public float speed;
     public float jumpforce;
+
     private float gravity = 25;
     private float verticalVelocity;
     private CharacterController controller;
@@ -15,19 +16,26 @@ public class PlayerControllerNewTest : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
+
+    //invisibility variables
+    public MeshRenderer playerMesh; //pull mesh from player model
+    float cooldownTimer = 1;
+    float invisibilityTimer = 1;
+
+    private bool cooldownActive = false; //is the cooldown active
+
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
     }
-    private void Update()
+
+    void Update()
     {
         moveVector = Vector3.zero;
         moveVector.x = Input.GetAxis("Horizontal");
@@ -56,9 +64,30 @@ public class PlayerControllerNewTest : MonoBehaviour
 
         controller.Move(moveVector * Time.deltaTime);
         lastMove = moveVector;
+
+        //if the player presses F
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+          //if cooldown is not on, run invisibility and set cooldown on
+          if (cooldownActive == false)
+          {
+            Invisible();
+            cooldownActive = true;
+          }
+
+          //if cooldown is on, wait for timer to run its course and then turn cooldown off
+          else if (cooldownActive != false)
+          {
+            cooldownTimer -= Time.deltaTime;
+            if (cooldownTimer <= 0)
+            {
+              cooldownActive = false;
+            }
+          }
+        }
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
+    void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (!controller.isGrounded && hit.normal.y < 0.1f)
         {
@@ -70,4 +99,18 @@ public class PlayerControllerNewTest : MonoBehaviour
         }
         Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);
     }
+
+    //function to use Invisibility
+    void Invisible()
+    {
+      //reduce alpha, go invisible
+      playerMesh.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+      //return to normal after a set time
+      invisibilityTimer -= Time.deltaTime;
+      if (invisibilityTimer <= 0)
+      {
+        playerMesh.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+      }
+    }
+
 }
