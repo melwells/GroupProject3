831218d90c8d440e3b5615db7a1e9dbf767f5f87
Javier Lopez <javierlopez@knights.ucf.edu>
2020-03-04 11:18:51 -0500

    Added Comments and Started Combat

--- GroupProject3_Test/Assets/Scripts/PlayerControllerNewTest.cs
+++ GroupProject3_Test/Assets/Scripts/PlayerControllerNewTest.cs
@@ -14,62 +14,75 @@ public class PlayerControllerNewTest : MonoBehaviour
     private CharacterController controller;
 
     public Transform groundCheck;
+    public Transform attackPoint;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>(); 
     }
 
     void FixedUpdate()
     {
-        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //Checks to see if object is on the ground
     }
 
     void Update()
     {
+        //Movement and Jumping
         moveVector = Vector3.zero;
-        moveVector.x = Input.GetAxis("Horizontal");
-        moveVector.z = Input.GetAxis("Vertical"); //z axis
+        moveVector.x = Input.GetAxis("Horizontal"); //X axis input
+        moveVector.z = Input.GetAxis("Vertical"); //Z axis input
 
-        moveVector = transform.right * moveVector.x + transform.forward * moveVector.z;
+        moveVector = transform.right * moveVector.x + transform.forward * moveVector.z; //Adjusts inputs to move character relative to the camera
 
         if (controller.isGrounded)
         {
-            verticalVelocity = -1;
+            verticalVelocity = -1; //Keeps character on the ground unless an input is provided
+
             if(Input.GetButtonDown("Jump"))
             {
-                verticalVelocity = jumpforce;
+                verticalVelocity = jumpforce; //Causes the character to jump
             }
         }
-        else
+        else //Slowly brings object back to the ground, and locks in direction of the jump
         {
             verticalVelocity -= gravity * Time.deltaTime;
             moveVector = lastMove;
         }
 
-        moveVector.y = 0;
+        moveVector.y = 0; //Resets y vector in case of lastMove
         moveVector.Normalize();
-        moveVector *= speed;
+        moveVector *= speed; //Adjusts how fast the character moves
         moveVector.y = verticalVelocity;
 
         controller.Move(moveVector * Time.deltaTime);
         lastMove = moveVector;
+
+        //Combat
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
+    //Wall Jumping
+    void OnControllerColliderHit(ControllerColliderHit hit) //Detects that the character is colliding with an object
     {
-        if (!controller.isGrounded && hit.normal.y < 0.1f)
+        if (!controller.isGrounded && hit.normal.y < 0.1f) //Checks that the character isn't grounded and also colliding with object that has a steep slope
         {
-            if(Input.GetButtonDown("Jump"))
+            if(Input.GetButtonDown("Jump")) //Bounces character away from the wall
             {
                 verticalVelocity = jumpforce;
                 moveVector = hit.normal * speed;
             }
         }
-        Debug.DrawRay(hit.point, hit.normal, Color.red, 1.25f);
     }
 
+    void Attack()
+    {
+
+    }
 }
